c2da96735cc27521be7a82f98356d7db431d7d1d
Sharan Selvaraj <sharselv@cisco.com>
2024-08-28 00:04:53 +0530

    Updating application code to make unit test pass for input with new line chars as separators

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 export function add(inputList: string) {
   if (inputList.length) {
-    const inputArray = inputList.split(",").map((strVal) => parseInt(strVal));
+    const inputArray = inputList
+      .split(/[,\n]/)
+      .map((strVal) => parseInt(strVal));
     return inputArray.reduce((sum, nextValue) => sum + nextValue, 0);
   }
   return 0;
