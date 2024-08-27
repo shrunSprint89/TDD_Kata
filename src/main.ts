7194d3ca4cd915591b60c4f7afbe3f7bba46907c
Sharan Selvaraj <sharselv@cisco.com>
2024-08-27 21:54:04 +0530

    Updating application code to make unit test pass for single number string with 1 as input

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
-export function calculateFromString(_inputList: string) {
+export function calculateFromString(inputList: string) {
+  if (inputList.length) {
+    return parseInt(inputList);
+  }
   return 0;
 }
