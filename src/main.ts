f92d49d18a3818518ed1d844dbc93d9f119c063b
Sharan Selvaraj <sharselv@cisco.com>
2024-08-27 22:35:05 +0530

    Updating application code to make unit test pass for 2 number strings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 export function calculateFromString(inputList: string) {
   if (inputList.length) {
-    return parseInt(inputList);
+    const inputArray = inputList.split(",").map((strVal) => parseInt(strVal));
+    if (inputArray.length === 1) {
+      return inputArray[0];
+    } else if (inputArray.length === 2) {
+      return inputArray[0] + inputArray[1];
+    }
   }
   return 0;
 }
