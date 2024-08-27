761cb5f51463935fdd3dab4d42bba6cd762cf78f
Sharan Selvaraj <sharselv@cisco.com>
2024-08-27 21:49:56 +0530

    Adding second unit test for string with 1 number as input

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,4 +5,8 @@ describe("calculateFromString", () => {
     const result = calculateFromString("");
     expect(result).toBe(0);
   });
+  it("return the same value for a string with 1 number as input", () => {
+    const result = calculateFromString("1");
+    expect(result).toBe(1);
+  });
 });
