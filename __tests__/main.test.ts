46395392678c6255f35ecc77866df348c2f93097
Sharan Selvaraj <sharselv@cisco.com>
2024-08-27 22:37:05 +0530

    Adding more unit tests for input of 2 number strings

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -20,5 +20,13 @@ describe("calculateFromString", () => {
       const result = calculateFromString("0,1");
       expect(result).toBe(1);
     });
+    it("return the sum as 100 for input as '5,95'", () => {
+      const result = calculateFromString("5,95");
+      expect(result).toBe(100);
+    });
+    it("return the sum as 20000 for input as '10000,10000'", () => {
+      const result = calculateFromString("10000,10000");
+      expect(result).toBe(20000);
+    });
   });
 });
