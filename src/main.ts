0e03db1de72b315909d716d8e28b5f8920088c7d
Sharan Selvaraj <sharselv@cisco.com>
2024-08-27 22:46:27 +0530

    Updating application code to make unit test pass for multi digit number strings

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 export function add(inputList: string) {
   if (inputList.length) {
     const inputArray = inputList.split(",").map((strVal) => parseInt(strVal));
-    if (inputArray.length === 1) {
-      return inputArray[0];
-    } else if (inputArray.length === 2) {
-      return inputArray[0] + inputArray[1];
-    }
+    return inputArray.reduce((sum, nextValue) => sum + nextValue, 0);
   }
   return 0;
 }
