cba6763ca55794bae7f29814d6a70b8bf46e7ac3
Sharan Selvaraj <sharselv@cisco.com>
2024-08-28 07:02:56 +0530

    Adding more unit test for support of different delimiters

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,59 +6,67 @@ describe("calculateFromString", () => {
     expect(result).toBe(0);
   });
   describe("Single digit string as input", () => {
-    it("return the sum as 1 for input as '1'", () => {
+    it("returns the sum as 1 for input as '1'", () => {
       const result = add("1");
       expect(result).toBe(1);
     });
-    it("return the sum as 9 for input as '9'", () => {
+    it("returns the sum as 9 for input as '9'", () => {
       const result = add("1");
       expect(result).toBe(1);
     });
   });
   describe("Two digit string as input", () => {
-    it("return the sum as 1 for input as '0,1'", () => {
+    it("returns the sum as 1 for input as '0,1'", () => {
       const result = add("0,1");
       expect(result).toBe(1);
     });
-    it("return the sum as 100 for input as '5,95'", () => {
+    it("returns the sum as 100 for input as '5,95'", () => {
       const result = add("5,95");
       expect(result).toBe(100);
     });
-    it("return the sum as 20000 for input as '10000,10000'", () => {
+    it("returns the sum as 20000 for input as '10000,10000'", () => {
       const result = add("10000,10000");
       expect(result).toBe(20000);
     });
   });
   describe("Multi digit string as input", () => {
-    it("return the sum as 1 for input as '0,0,0,0,0,0,0,1'", () => {
+    it("returns the sum as 1 for input as '0,0,0,0,0,0,0,1'", () => {
       const result = add("0,0,0,0,0,0,0,1");
       expect(result).toBe(1);
     });
-    it("return the sum as 636 for input as '1,2,3,4,5,6,7,8,100,200,300'", () => {
+    it("returns the sum as 636 for input as '1,2,3,4,5,6,7,8,100,200,300'", () => {
       const result = add("1,2,3,4,5,6,7,8,100,200,300");
       expect(result).toBe(636);
     });
-    it("return the sum as 22000 for input as '10,10,10,9,1,8,2,100,50,800,1000,5000,5000,5000,5000'", () => {
+    it("returns the sum as 22000 for input as '10,10,10,9,1,8,2,100,50,800,1000,5000,5000,5000,5000'", () => {
       const result = add(
         "10,10,10,9,1,8,2,100,50,800,1000,5000,5000,5000,5000"
       );
       expect(result).toBe(22000);
     });
-    it("return the sum as 10000 for input as string with 1000 numbers of 10s", () => {
+    it("returns the sum as 10000 for input as string with 1000 numbers of 10s", () => {
       const inputString = new Array(1000).fill(10).join(",");
       const result = add(inputString);
       expect(result).toBe(10000);
     });
   });
   describe("Multi digit string as input with new line chars as separators", () => {
-    it("return the sum as 10 for input as '1,1\\n1,1,1\\n1,1,3'", () => {
+    it("returns the sum as 10 for input as '1,1\\n1,1,1\\n1,1,3'", () => {
       const result = add("1,1\n1,1,1\n1,1,3");
       expect(result).toBe(10);
     });
   });
   describe("Multi digit string as input with any delimiter mentioned in input string itself", () => {
-    it("return the sum as 10 for input as '//;\\n1;2\\n1;1;1\\n1;1;3'", () => {
+    it("returns the sum as 10 for input as '//;\\n1;2\\n1;1;1\\n1;1;3'", () => {
       const result = add("//;\n1;1\n1;1;1\n1;1;3");
+      expect(result).toBe(10);
+    });
+    it("returns the sum as 10 for input as '//#\\n1#1\\n1#1#1\\n1#1#3'", () => {
+      const result = add("//#\n1#1\n1#1#1\n1#1#3");
+      expect(result).toBe(10);
+    });
+    it("returns the sum as 10 for input as '//#\\n1#1#1#1#1#1#1#3'", () => {
+      const result = add("//#\n1#1\n1#1#1\n1#1#3");
       expect(result).toBe(10);
     });
   });
